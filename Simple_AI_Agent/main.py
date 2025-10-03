015776c9e4fdfb71932e9a69b1d80b300ce12fa2
Anh Duc Nguyen <nguyenducanh4statistics.gmail.com>
2025-10-03 13:22:06 +0200

    Simple_AI_Agent 1310cet 3Oct25

---
+++ Simple_AI_Agent/main.py
@@ -0,0 +1,6 @@
+from langchain_core.messages import HumanMessage  # highe level framework to build AI applications
+from langchain_openai import ChatOpenAI # to use open AI within langchain and langgraph
+from langchain.tools import tool
+from langgraph.prebuilt import create_react_agent # complex framework to build AI agents
+from dotenv import load_dotenv # load environment variables in .env file from within python script
+
